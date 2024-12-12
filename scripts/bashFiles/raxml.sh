2961994750c257d86f104a9be814ad5902df9be4
vannguyenh <van.nguyenhoang@mail.com>
2024-12-12 14:07:40 +1100

    add bash files

---
+++ scripts/bashFiles/raxml.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RF=$1
+FASTA_FILE=$2
+PREFIX=$3
+RAXML=$4
+
+mkdir -p $PREFIX
+
+for i in {1..10}; do
+    formatted_i=$(printf "%02d" $i)
+    $RAXML -s $FASTA_FILE -n ${RF}.${formatted_i} -m GTRGAMMA -p $i -w ${PREFIX}
+done
