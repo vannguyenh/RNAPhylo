b2ae3efd30951ac2ac66679e2d08e38f05026991
vannguyenh <van.nguyenhoang@mail.com>
2025-05-07 11:06:07 +1000

    remove not-required script to download .sto files which is in the wrong directory

--- data/CURRENT_full_alignments/download_sto_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..4310}; do
-  formatted_i=$(printf "%05d" $i)
-  rf_file=${RF}.{formatted_i}
-  wget https://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/full_alignments/${rf_file}.sto
-done
