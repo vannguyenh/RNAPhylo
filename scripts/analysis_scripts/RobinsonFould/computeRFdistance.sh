a4793016478bba210cff88e85f64d9e3d24983aa
Van Nguyen <48513648+vannguyenh@users.noreply.github.com>
2025-11-21 13:21:13 +1100

    only one line to compute the RF distances between DNA vs DNA_extra or DNA vs RNA

--- scripts/analysis_scripts/RobinsonFould/computeRFdistance.sh
+++ scripts/analysis_scripts/RobinsonFould/computeRFdistance.sh
@@ -6,12 +6,6 @@ raxPiPtree=$2
 prefix=$3
 rna=$4
 
-IQTREE="/Users/u7875558/Documents/promotion/projects/projects_code/iqtree3/build/iqtree3"
+IQTREE="/Users/u7875558/tools/build-iqtree3/iqtree3"
 
-${IQTREE} -rf_all ${raxtree}
-#${IQTREE} -nt 1 -rf_all ${raxPwPtree}
-${IQTREE} -rf_all ${raxPiPtree}
-
-#${IQTREE} -rf ${raxtree} ${raxPwPtree} --prefix ${prefix}/${rna}.raxml.raxmlPw
-${IQTREE} -rf ${raxtree} ${raxPiPtree} --prefix ${prefix}/${rna}.raxml.raxmlPi
-#${IQTREE} -rf ${raxPwPtree} ${raxPiPtree} --prefix ${prefix}/${rna}.raxmlPw.raxmlPi
+${IQTREE} -rf ${raxtree} ${raxPiPtree} --prefix ${prefix}/${rna}
