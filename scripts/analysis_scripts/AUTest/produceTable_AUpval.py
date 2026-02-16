1f6ced8ed32667320cfff3565192ae2a946f09e8
Van Nguyen <48513648+vannguyenh@users.noreply.github.com>
2026-02-16 15:54:45 +1100

    create two csv files for DNA p val and RNA p val

--- scripts/analysis_scripts/AUTest/produceTable_AUpval.py
+++ scripts/analysis_scripts/AUTest/produceTable_AUpval.py
@@ -10,10 +10,7 @@ import os
 from os import listdir
 from os.path import join, isdir
 import subprocess
-import numpy as np
 import pandas as pd
-from Bio import Phylo
-from statsmodels.stats.multitest import multipletests
 
 # ─── USER PARAMETERS ───────────────────────────────────────────────────────────
 
@@ -24,7 +21,7 @@ DIR_OUTPUTS     = join(DIR_WORKING, 'outputs')
 DIR_AU = join(DIR_OUTPUTS, '260208_AU_Test_RAxML_RNAmodels')
 
 EXPECTED_SEEDS = {f"{i:02d}" for i in range(1, 11)}
-SUFFIX_IGNORE = '_consel'  # suffix before .pv for ignore_pseudo
+SUFFIX_IGNORE = 'consel'  # suffix before .pv for ignore_pseudo
 
 # ─── UTILITY FUNCTIONS ────────────────────────────────────────────────────────
 
@@ -54,15 +51,15 @@ def main():
     # Discover S6*/S16*/S7* models
     models = sorted(
         m for m in listdir(DIR_AU)
-        if isdir(join(DIR_AU, m)) and ((m.startswith('S') or (m.startswith('extra'))))
+        if isdir(join(DIR_AU, m)) and (m.startswith('S') or (m.startswith('extra')))
     )
 
     records = []
 
     for model in models:
-        consel_ignore = join(DIR_AU, model, 'ignore_pseudo')
-        for rna in listdir(consel_ignore):
-            pv_file = join(consel_ignore, rna, f"{rna}_{SUFFIX_IGNORE}.pv")
+        consel_model= join(DIR_AU, model)
+        for rna in listdir(consel_model):
+            pv_file = join(consel_model, rna, f"{rna}_{SUFFIX_IGNORE}.pv")
             if not os.path.exists(pv_file):
                 continue
             p_raw = parse_consel_output(pv_file)
@@ -72,14 +69,20 @@ def main():
     # Build DataFrame and pivot to wide format
     df = pd.DataFrame(records)
     print(df.head())
-    full_csv = join(DIR_AU, '260204_AU_pValues_bothpVals_SEED.csv')
+    full_csv = join(DIR_AU, '260210_AU_pValues_both_SEED.csv')
     df.to_csv(full_csv, sep='\t')
 
     df_table = df.pivot(index='RNA_family', columns='Model', values='p_RNA_raw').sort_index()
     print(df_table.head())
-    out_csv = join(DIR_AU, '260208_AUpvalues_RNApVal_SEED.csv' )
-    df_table.to_csv(out_csv)
-    print(f"Saved p-values from AU test of all models to: {full_csv}, {out_csv}")
+    rna_csv = join(DIR_AU, '260208_AUpvalues_RNApVal_SEED.csv' )
+    df_table.to_csv(rna_csv)
+    print(f"Saved p-values from AU test of all models to: {full_csv}, {rna_csv}")
+
+    df_table = df.pivot(index='RNA_family', columns='Model', values='p_DNA_raw').sort_index()
+    print(df_table.head())
+    dna_csv = join(DIR_AU, '260208_AUpvalues_DNApVal_SEED.csv')
+    df_table.to_csv(dna_csv)
+    print(f"Saved p-values from AU test of all models to: {full_csv}, {dna_csv}")
 
 if __name__ == '__main__':
     main()
