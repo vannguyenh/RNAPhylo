e584628b90d088aafa2c606de4519d28e770237e
Van Nguyen <48513648+vannguyenh@users.noreply.github.com>
2026-02-16 15:56:18 +1100

    comment out the case of dna vs dna

--- scripts/analysis_scripts/RobinsonFould/computeRFdistance.sh
+++ scripts/analysis_scripts/RobinsonFould/computeRFdistance.sh
@@ -9,3 +9,5 @@ rna=$4
 IQTREE="/Users/u7875558/tools/build-iqtree3/iqtree3"
 
 ${IQTREE} -rf ${raxtree} ${raxPiPtree} --prefix ${prefix}/${rna}
+# full alignment
+# ${IQTREE} -rf_all ${raxtree} --prefix ${prefix}/${rna}_dna
