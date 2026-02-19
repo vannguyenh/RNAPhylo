af3234dab261a73456f241ac599d2362a1f320e7
Van Nguyen <48513648+vannguyenh@users.noreply.github.com>
2026-02-19 16:35:20 +1100

    run 10 tree inferences and make sure wait until all is done

--- scripts/nci_scripts/bashFiles/raxml.sh
+++ scripts/nci_scripts/bashFiles/raxml.sh
@@ -6,8 +6,11 @@ PREFIX=$3
 RAXML=$4
 
 mkdir -p $PREFIX
-
+# Run all 10 in parallel
 for i in {11..20}; do
     formatted_i=$(printf "%02d" $i)
-    $RAXML -s $FASTA_FILE -n ${RF}.${formatted_i} -m GTRGAMMA -p $i -w ${PREFIX} #qsub command line here. -- each command line takes 1 cpus.
+    $RAXML -s $FASTA_FILE -n ${RF}.${formatted_i} -m GTRGAMMA -p $i -w ${PREFIX} & #qsub command line here. -- each command line takes 1 cpus.
 done
+
+# Wait for all to finish
+wait
